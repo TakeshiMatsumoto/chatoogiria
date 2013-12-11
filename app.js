5f5c34a50bd4dec35f88df9013e5979d6d000b99
bonkun <take99matsu@yahoo.co.jp>
2013-12-11 23:46:59 +0900

    kkk

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.configure(function(){//アプリケー主恩の編集
   app.set('views', __dirname + '/views');
   app.use(express.static(__dirname + '/static'));
   app.set('view engine', 'ejs');
-     app.use(express.static(__dirname + '/static'));
+  app.use(express.static(__dirname + '/static'));
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
